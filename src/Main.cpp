45ea49db497466011f2b12d5e03abc546e1c6652
Anthony-Kuismi <arkuismi@outlook.com>
2022-09-22 13:59:01 -0500

    Changing input to a 1d array

--- src/Main.cpp
+++ src/Main.cpp
@@ -25,8 +25,8 @@ void printArray2D(double *arr, int width, int height);
 void clearArray2D(double *arr, int width, int height);
 
 const int INPWIDTH = 24, INPHEIGHT = 32, LAYER2NEURONS = 16, LAYER3NEURONS = 16, OUTPUTNUERONS = 10, PIXELWIDTH = 20;
-double input[INPHEIGHT][INPWIDTH],w1[LAYER2NEURONS][INPHEIGHT*INPWIDTH],w2[LAYER3NEURONS][LAYER2NEURONS],w3[OUTPUTNUERONS][LAYER3NEURONS];
-
+double input[INPHEIGHT*INPWIDTH],w1[LAYER2NEURONS][INPHEIGHT*INPWIDTH],w2[LAYER3NEURONS][LAYER2NEURONS],w3[OUTPUTNUERONS][LAYER3NEURONS];
+double a2[LAYER2NEURONS],a3[LAYER3NEURONS],b2[LAYER2NEURONS],b3[LAYER3NEURONS];
 
 int main(int argc, char** argv){
     init();
@@ -67,16 +67,16 @@ void mainLoop(){
         drawAtMouse();
     }
 }
-
+//clears the input
 void clear(){
-    clearArray2D(*input,INPWIDTH,INPHEIGHT);
+    clearArray1D(input,INPWIDTH*INPHEIGHT);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer,0,0,0,255);
     SDL_RenderFillRect(renderer,&rect);
     SDL_RenderPresent(renderer);
 }
-//
+//Draws a square at the mouse position
 void drawAtMouse(){
     int mx,my;
     Uint32 buttons;
@@ -85,24 +85,29 @@ void drawAtMouse(){
     if ((buttons & SDL_BUTTON_LMASK) != 0) {
         //turns mouse click into a 10x10 pixel
         SDL_Rect r;
+        int xConversion = mx/PIXELWIDTH;
+        int yConversion = my/PIXELWIDTH;
         r.h = PIXELWIDTH;
         r.w = PIXELWIDTH;
-        r.x = PIXELWIDTH*(mx/PIXELWIDTH);
-        r.y = PIXELWIDTH*(my/PIXELWIDTH);
+        r.x = PIXELWIDTH*xConversion;
+        r.y = PIXELWIDTH*yConversion;
         if(mx < rect.w && my < rect.h){
             SDL_SetRenderDrawColor(renderer,0,255,0,255);
             SDL_RenderFillRect(renderer,&r);
             SDL_RenderPresent(renderer);
-            input[my/PIXELWIDTH][mx/PIXELWIDTH] = 1;
+            //adds mouseclick to input.
+            input[INPWIDTH*yConversion+xConversion] = 1;
         }
     }
 }
 //solves and finds the output
 void solve(){
-    printArray2D(*input,INPWIDTH,INPHEIGHT);
+    //computeNextLayer(*w1,INPWIDTH*INPHEIGHT,INPWIDTH*INPHEIGHT ,*input,LAYER2NEURONS, b2,LAYER2NEURONS, a3,LAYER2NEURONS);
+    printArray1D(input,INPWIDTH*INPHEIGHT);
 }
 //takes in a the weights, activation amounts of previous neurons, and the biases.
-double** computeNextLayer(double weights, double** activations, double* biases){
+void computeNextLayer(double *weights,int weightsWidth,int weightsHeight, double *activations, int activationsSize, double *biases,int biasSize, double *nextLayer, int nextLayerSize){
+    //take the sigmoid of the vector multiplation of the weights and the activation and add the bias
     
 }
 //clears a 2D array
@@ -136,3 +141,4 @@ void printArray1D(double *arr, int size){
     }
     cout << endl << endl;
 }
+
